c3efa28c2f1509b477590b75d3126327aa9ebdc7
daoming.lzh <daoming.lzh@alibaba-inc.com>
2021-12-11 08:50:24 +0800

    使用9.0tomcat

--- mv.sh
+++ mv.sh
@@ -1,2 +1,2 @@
 mvn clean package -Dmaven.test.skip=true
-mv target/springdemo.war /Users/liuzhenhui/Envirment/tomcat-10.0.13/webapps/
+mv target/springdemo.war /Users/liuzhenhui/Envirment/tomcat-9.0.56/webapps/
