5c55a2d9872ca0d980a77195e498df3dde4ff768
daoming.lzh <daoming.lzh@alibaba-inc.com>
2021-12-28 20:31:09 +0800

    注释

--- src/main/java/lzh/example/start/listener/bean/BeanConfig.java
+++ src/main/java/lzh/example/start/listener/bean/BeanConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * @Author daoming.lzh
  * @Date: 2021/12/17 下午12:42
  * bean的初始化顺序
+ * https://juejin.cn/post/6844904142750613511
  * https://mp.weixin.qq.com/s?__biz=MzkwNzI0MzQ2NQ==&mid=2247488902&idx=1&sn=f3ba0e4925ad9293ec69cecf8916acb6&source=41#wechat_redirect
  */
 @Configuration
